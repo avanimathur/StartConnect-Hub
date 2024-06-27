a340b8e2525af83a33f08946f1d8b4a79ad05a3b
avanimathur <avanimathur24@gmail.com>
2024-06-27 15:56:33 +0530

    investor page

--- src/Pages/InvestorManagementPage.jsx
+++ src/Pages/InvestorManagementPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import './startup.css'
 import { NavLink } from 'react-router-dom';
-import StartUps from './startUps';
-
 const InvestorManagementPage = () => {
   return (
     <ManagementPageWrapper>
@@ -14,6 +11,8 @@ const InvestorManagementPage = () => {
     We understand the challenges that startups face, from securing funding to navigating the complexities of the business world. 
     With our platform, entrepreneurs can bring their visions to life and take their businesses to new heights.
     </section>
+
+    <a href="./Pages/StarUps.jsx"><Button className="view-statups">View Startups</Button></a>
 
     <h2 className='sub-heading'>
     Why Choose StartConnectHub?
